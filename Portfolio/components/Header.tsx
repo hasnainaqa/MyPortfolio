0b6768f95c0d97986abe3284480c42dee9d1357f
hasnainaqa <151780764+hasnainaqa@users.noreply.github.com>
2025-01-01 03:26:21 +0500

    Update Header.tsx

--- Portfolio/components/Header.tsx
+++ Portfolio/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="fixed bg-white shadow-sm w-screen">
       <nav className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <Link href="/" className="text-2xl font-bold text-blue-600 ml-16">HAQA</Link>
+        <Link href="https://www.instagram.com/hasnain.aqa" className="text-2xl font-bold text-blue-600 ml-16">H.A.Q.A</Link>
         <ul className="flex space-x-4">
           <li><Link href="#about" className="text-gray-600 hover:text-blue-600">About</Link></li>
           <li><Link href="#skills" className="text-gray-600 hover:text-blue-600">Skills</Link></li>
